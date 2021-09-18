fd3e1bfb3e5c923056e50b53bc703b3bd4f0fa6a
Borys Khoroshyi <boria.good@gmail.com>
2021-09-18 12:41:06 +0300

    fixed issues/19

--- src/screens/SplashScreenAnimation/styles.ts
+++ src/screens/SplashScreenAnimation/styles.ts
@@ -4,18 +4,19 @@ import {width} from '@constants';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    width,
+    // width,
     alignItems: 'center',
     backgroundColor: '#009F30',
   },
   lottie: {
-    width,
+    // width,
     flex: 1,
     ...Platform.select({
       android: {
         transform: [{scaleX: 1.08}, {scaleY: 1.08}],
         top: -27,
       },
+      ios: {},
     }),
   },
 });
