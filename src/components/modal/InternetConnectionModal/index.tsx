e065fe4559bff3cfef165f5aba9dc7b99897f978
viktoriya Saveleva <v.akimova@artjoker.team>
2023-06-21 14:02:00 +0300

    refactor: InternetConnectionModal

--- src/components/modal/InternetConnectionModal/index.tsx
+++ src/components/modal/InternetConnectionModal/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import {useTranslation} from '@hooks';
-import {View, Text, Image, Modal} from '@components';
-import styles from './styles';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
+
+import {useTranslation} from '@hooks';
+import {View, Text, Image, Modal, LinearGradient} from '@components';
 import {assets} from '@assets';
+import {gradients} from '@constants';
+
+import styles from './styles';
 
 type TProps = {
   dispatch: Dispatch;
   isConnected: boolean;
 };
+
+const LOCATIONS = [0.1, 0.2, 0.7];
 
 const InternetConnectionModal: React.FC<TProps> = ({dispatch, isConnected}) => {
   const {t} = useTranslation();
@@ -19,15 +24,29 @@ const InternetConnectionModal: React.FC<TProps> = ({dispatch, isConnected}) => {
       isVisible={!isConnected}
       style={styles.container}
       backdropTransitionOutTiming={0}>
-      <View style={styles.noInternetContainer}>
-        <Text style={styles.noInternetTitle}>
-          {t('Проблеми з мережею Інтернет')}
-        </Text>
-        <Image source={assets.NO_INTERNET} style={styles.noInternetImage} />
-        <Text style={styles.noInternetDescription}>
-          {t(`Будь ласка перевірте з'єднання з Інтернетом`)}
-        </Text>
-      </View>
+      <LinearGradient
+        colors={gradients.red}
+        locations={LOCATIONS}
+        style={styles.gradient}>
+        <View style={styles.noInternetContainer}>
+          <View style={styles.noInternetTitleContainer}>
+            <Text style={styles.noInternetTitle}>
+              {t('Проблеми з мережею Інтернет')}
+            </Text>
+          </View>
+          <View style={styles.noInternetImageContainer}>
+            <Image
+              source={assets.NO_CONNECTION}
+              style={styles.noInternetImage}
+            />
+          </View>
+          <View style={styles.noInternetDescriptionContainer}>
+            <Text style={styles.noInternetDescription}>
+              {t(`Будь ласка перевірте підключення до інтерету`)}
+            </Text>
+          </View>
+        </View>
+      </LinearGradient>
     </Modal>
   );
 };
