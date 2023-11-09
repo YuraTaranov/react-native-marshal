3d3dc2dc4c9bb7c34d72cda84e3ab159042ba8a3
Daniil <d.bakanov@artjoker.net>
2023-11-09 14:13:13 +0200

    test ios build

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,25 +1,23 @@
 #!/bin/zsh
 
- # fail if any command fails
+# fail if any command fails
 
- echo " Stage: Post-clone is activated .... "
+echo " Stage: Post-clone is activated .... "
 
- set -e
- # debug log
- set -x
+set -e
+# debug log
+set -x
 
- # Install dependencies using Homebrew. This is MUST! Do not delete.
- brew install node@16 yarn cocoapods fastlane
+# Install dependencies using Homebrew. This is MUST! Do not delete.
+brew search node
+brew install node@16 yarn cocoapods fastlane
+brew link node@16
+node --version
+# Install yarn and pods dependencies.
+# If you're using Flutter or Swift
+# just install pods by "pod install" command
+ls && cd .. && yarn && pod install
  
- echo 'export PATH="/usr/local/opt/node@16/bin:$PATH"' >> ~/.zshrc
- export LDFLAGS="-L/usr/local/opt/node@16/lib"
- export CPPFLAGS="-I/usr/local/opt/node@16/include"
+echo " Stage: Post-clone is done .... "
 
- # Install yarn and pods dependencies.
- # If you're using Flutter or Swift
- # just install pods by "pod install" command
- ls && cd .. && yarn && pod install
- node --version
- echo " Stage: Post-clone is done .... "
-
- exit 0
+exit 0
